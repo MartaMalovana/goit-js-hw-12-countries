1116ad73f6edd9f24c687621ee29a3bd16aff657
MartaMalovana <marta.malovana@gmail.com>
2021-11-07 23:23:02 +0200

    Робота над pnotify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './sass/main.scss';
+import a from 'lodash.debounce';
+
+import {notice} from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/Material.css';
+
+const inputRef = document.querySelector('[name="country"]');
+
+const onCountryNameSearch = function () {
+  const inputValue = inputRef.value;
+  console.log(inputValue);
+
+  const filterResult = function (searchResult) {
+    if(searchResult.length > 5) {
+      return notice ({
+        title: 'Too many matches found.',
+        text: 'Please enter a more specific query!',
+        animation: 'none',
+      });
+    };
+  };
+
+  const searchCountryByName = function (name) {
+    const url = `https://restcountries.com/v2/name/${name}`;
+    return fetch(url).then(a => a.json()).then(filterResult).catch(()=>console.log('bad'));
+  };
+
+  searchCountryByName (inputValue); 
+
+};
+
+inputRef.addEventListener('input', a(onCountryNameSearch, 500));
+
